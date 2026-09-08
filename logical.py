c4a324d4d560b8b15c2327f47412ceacf7dfd731
chlamathigarikeerthi <chlamathigarikeerthi@gmail.com>
2026-09-08 10:29:39 +0530

    Add files via upload

--- logical.py
+++ logical.py
@@ -1,10 +1,10 @@
 #logical operators
 a = 25
 citizen = True
-print(age >= 18 and citizen == True)
-age = 16
+print(a >= 18 and citizen==True)
+a = 16
 citizen = True
-print(age >= 18 and citizen == True)
+print(a >= 18 and citizen==True)
 has_card = False
 has_cash = True
 print(has_card or has_cash )
@@ -19,3 +19,25 @@ marks = float(input("Enter your marks: "))
 attendance = float(input("Enter your attendance: "))
 eligible = marks >= 85 and attendance >= 75
 print("Scholarship Eligibility:", eligible)
+#identity operators
+a = None
+print(a is None)
+print(a is not None)
+#bitwise operators
+a = 5
+b = 3
+print(a & b)  
+print(a | b)
+print(a ^ b) 
+#electric city bill calculator
+units = int(input("Enter electricity units: ")) 
+rate = 6
+bill = units * rate
+print("Electricity Bill:", bill) 
+# travel expense calculator
+travel = float(input("Travel expense: "))
+food = float(input("Food expense: "))
+hotel = float(input("Hotel expense: "))
+total = travel + food + hotel
+print("Total Expense:", total)
+ 
